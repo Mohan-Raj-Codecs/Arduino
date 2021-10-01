ff0ff12aee26f9472162a61737d54324a7f55f5c
Mohan Raj <74151223+Mohan-Raj-Codecs@users.noreply.github.com>
2021-10-01 21:51:40 +0530

    Create IR_to_AC_Control - 2.ino

---
+++ IR_to_AC_Control/IR_to_AC_Control - 2.ino
@@ -0,0 +1,27 @@
+int inp=A0;
+float input,volt;
+int out=13;
+int bulb=A1;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(inp,INPUT);
+  pinMode(out,OUTPUT);
+  pinMode(bulb,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(bulb,0);
+  digitalWrite(out,LOW);
+  input=analogRead(inp);
+  volt=(input*5.0)/1024.0;
+  Serial.print("voltage is : ");
+  Serial.println(volt);
+  if(volt<1){
+    digitalWrite(out,HIGH);
+    analogWrite(bulb,150);
+    delay(3000);
+    }
+
+}
