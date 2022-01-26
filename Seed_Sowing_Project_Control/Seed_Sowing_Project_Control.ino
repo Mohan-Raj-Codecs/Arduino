885fd1d33e588c3306637612c2cb448ceb2ca9ed
Mohan Raj <74151223+Mohan-Raj-Codecs@users.noreply.github.com>
2022-01-27 00:45:17 +0530

    Update Seed_Sowing_Project_Control.ino

--- Seed_Sowing_Project_Control/Seed_Sowing_Project_Control.ino
+++ Seed_Sowing_Project_Control/Seed_Sowing_Project_Control.ino
@@ -6,18 +6,20 @@ int IR_RECEIVE_PIN = 11;
 
 //Vars
 const int voli4 = 8208; const int vold4 = 8209; const int rch4 = 8224; const int lch4 = 8225; 
-const int ok4 = 8240; const int power4 = 8204; const int mute4 = 8205;
+const int ok4 = 8240; const int power4 = 8204; const int mute4 = 8205; const int selec = 9999;
 int ir_code = 0, last_ms = millis();
-bool wheel=false; bool spray=false; bool seed=false; bool grass=false;
+bool wheel_f=false; bool wheel_b=false; bool spray=false; bool seed=false; bool grass=false;
 //PINS
-int wheel_motor=3; int spray_motor=5; int seed_motor=6; int grass_motor=9;
+int wheel_motor_f=3; int wheel_motor_b=4; int spray_motor=5; int seed_motor=6;int seed_motor_pwm=10; int grass_motor=9;
 
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(wheel_motor,OUTPUT);
+  pinMode(wheel_motor_f,OUTPUT);
+  pinMode(wheel_motor_b,OUTPUT);
   pinMode(spray_motor,OUTPUT);
   pinMode(seed_motor,OUTPUT);
+  pinMode(seed_motor_pwm,OUTPUT);
   pinMode(grass_motor,OUTPUT);
   Serial.begin(9600);   // Status message will be sent to PC at 9600 baud
 #if defined(__AVR_ATmega32U4__) || defined(SERIAL_USB) || defined(SERIAL_PORT_USBVIRTUAL)  || defined(ARDUINO_attiny3217)
@@ -48,13 +50,14 @@ void loop() {
     //}
 
     switch (ir_code) {
-      case voli4: Serial.println("Vol Up"); wheel=inv(wheel); break;
-      case vold4: Serial.println("Vol Down"); grass=inv(grass); break;
+      case voli4: Serial.println("Vol Up"); wheel_f=inv(wheel_f); wheel_b=false; break;
+      case vold4: Serial.println("Vol Down"); wheel_b=inv(wheel_b); wheel_f=false; break;
+      case selec: Serial.println("Select"); grass=inv(grass); break;
       case rch4: Serial.println("Right Channel"); spray=inv(spray);break;
       case lch4: Serial.println("Left Channel"); seed=inv(seed); break;
-      case ok4: Serial.println("OK"); seed=true; wheel=true; spray=true; grass=true;  break;
-      case power4: Serial.println("Power"); seed=false; wheel=false; spray=false; grass=false; break;
-      case mute4: Serial.println("Mute"); seed=true; wheel=true; spray=false; grass=false; break;
+      case ok4: Serial.println("OK"); seed=true; wheel_f=true; wheel_b=false; spray=true; grass=true;  break;
+      case power4: Serial.println("Power"); seed=false; wheel_f=false; wheel_b=false; spray=false; grass=false; break;
+      case mute4: Serial.println("Mute"); seed=true; wheel_f=true; wheel_b=false; spray=false; grass=false; break;
       default: Serial.println("Unknown Button");
     }
     print_data();
@@ -87,8 +90,10 @@ bool isRepeat(int current_code){
 }
 
 void print_data(){
-  Serial.print("\nwheel: ");
-  Serial.println(wheel);
+  Serial.print("\nwheel forward: ");
+  Serial.println(wheel_f);
+  Serial.print("\nwheel backward: ");
+  Serial.println(wheel_b);
   Serial.print("spray: ");
   Serial.println(spray);
   Serial.print("seed: ");
@@ -99,8 +104,28 @@ void print_data(){
 }
 
 void update_motor(){
-  digitalWrite(wheel_motor,(wheel)?HIGH:LOW);
+  digitalWrite(wheel_motor_f,(wheel_f)?HIGH:LOW);
+  if((wheel_f!=wheel_b) || (wheel_b==false)){
+    digitalWrite(wheel_motor_b,(wheel_b)?HIGH:LOW);
+  }
+  else{
+    Serial.println("Motor Short Protection :( ");
+  }
   digitalWrite(spray_motor,(spray)?HIGH:LOW);
-  digitalWrite(seed_motor,(seed)?HIGH:LOW);
+  seeder();
+  //digitalWrite(seed_motor,(seed)?HIGH:LOW);
   digitalWrite(grass_motor,(grass)?HIGH:LOW);
+}
+
+void seeder(){
+  if(seed){
+    //delay(200);
+    //digitalWrite(seed_motor,LOW);
+    analogWrite(seed_motor_pwm,80);
+    digitalWrite(seed_motor,HIGH);
+  }
+  else{
+    digitalWrite(seed_motor,LOW);
+    analogWrite(seed_motor_pwm,0);
+  }  
 }
